2d2680da91663e67c2525a472749ccde61612bf7
Vladrrs <vladtodor13@yahoo.com>
2026-04-23 12:21:46 +0300

    Refactor guess_the_number.cpp for clarity and comments

--- Guess the number/guess_the_number.cpp
+++ Guess the number/guess_the_number.cpp
@@ -1,34 +1,45 @@
-#include <iostream>
-#include <cstdlib> 
-#include <ctime>    
+#include <iostream>   // pentru input (cin) si output (cout)
+#include <cstdlib>    // pentru functia rand()
+#include <ctime>      // pentru time(), folosit la random
 
 using namespace std;
 
 int main()
 {
+    // Initializam generatorul de numere random
+    // time(0) face ca numerele sa fie diferite de fiecare data
     srand(time(0)); 
 
-    
+    // Generam un numar aleator intre 1 si 100
     int randomnumber = rand() % 100 + 1; 
+
+    // attempt = numarul de incercari
+    // attemptuser = numarul introdus de utilizator
     int attempt = 0, attemptuser = 0;
 
+    // Mesaj pentru utilizator
     cout << "Ghiceste numarul (intre 1 si 100)" << endl;
 
+    // Bucla care se repeta pana ghicesti numarul
     do {
         cout << "Ghiceste: ";
-        cin >> attemptuser;
-        attempt++;
+        cin >> attemptuser;   // citim numarul de la tastatura
 
+        attempt++; // crestem numarul de incercari
+
+        // Verificam daca numarul este prea mic
         if (attemptuser < randomnumber) {
             cout << "Prea mic, mai incearca" << endl;
         } 
+        // Verificam daca numarul este prea mare
         else if (attemptuser > randomnumber) {
             cout << "Prea mare, mai incearca" << endl;
         }
 
-    } while (attemptuser != randomnumber);
+    } while (attemptuser != randomnumber); // continua pana ghicesti corect
 
-    cout << "Correct! Numarul de incercari: " << attempt << endl;
+    // Cand ghicesti corect
+    cout << "Corect! Numarul de incercari: " << attempt << endl;
 
-    return 0;
+    return 0; // programul se termina
 }
